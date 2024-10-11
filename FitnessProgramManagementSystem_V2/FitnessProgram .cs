f1ed53b0f641d5db64f1a70165db2d5ebac83ce8
ut01170 <ut01170@UNICOMTIC14>
2024-10-11 12:02:00 +0530

    1st commit- project created

---
+++ FitnessProgramManagementSystem_V2/FitnessProgram .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProgramManagementSystem_V2
+{
+    internal class FitnessProgram
+    {
+        public string fitnessProgramId { get; set; }
+        public string title { get; set; }
+        public string duration { get; set; }
+        public decimal price { get; set; }
+
+    }
+}
--- FitnessProgramManagementSystem_V2/Program.cs
+++ FitnessProgramManagementSystem_V2/Program.cs
@@ -10,6 +10,22 @@ namespace FitnessProgramManagementSystem_V2
     {
         static void Main(string[] args)
         {
+
+        }
+
+        static void SetConnection()
+        {
+            string masterDbConnectionString = "Server=(localdb)\\MSSQLLocalDB; Database=master; Trusted_Connection=True; TrustServerCertificate=True;";
+            string DbConnectionString = "Server=(localdb)\\MSSQLLocalDB; Database=FitnessProgramManagement; Trusted_Connection=True; TrustServerCertificate=True;";
+
+            string dbQuery = @"
+                                IF NOT EXISTS (SELECT * FROM sys.databases WHERE name='FitnessProgramManagement')
+                                CREATE DATABASE FitnessProgramManagement;";
+            string tableQuery = @"
+                                IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='FitnessPrograms' AND xtype='U')
+                                CREATE TABLE FitnessPrograms(
+                                );";
+
         }
     }
 }
